49f647152a79f20d04093c190c4f152983195ea0
Georgekay <amoakye@yahoo.com>
2025-12-03 02:14:59 +0100

    controllers,repository, and authentication modified

---
+++ src/main/java/com/kay/driving_exam_app/mapper/mapperConfig.java
@@ -0,0 +1,10 @@
+package com.kay.driving_exam_app.mapper;
+
+import org.modelmapper.ModelMapper;
+
+public class mapperConfig {
+ public ModelMapper mapper()
+ {
+     return new ModelMapper();
+ }
+}
